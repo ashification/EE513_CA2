a0d5a59fcdac7df59d26e72ba6a89ba1e36ce350
ashification <aislinglee@gmail.com>
2021-04-11 22:31:38 +0100

    Test Broekr Push

---
+++ CA2_Commit.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [ "$1" == "" ]; then
+    printf  "ERROR: First Arg is empty, Please Provide Commit Comment"
+else
+        git add .
+        git commit -m "$1"
+        git push origin BROKER_Code
+        printf "Code Comitted. Please Verify\n"
+fi
