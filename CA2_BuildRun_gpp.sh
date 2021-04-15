bf29e159115c867040a59fa1bf6e01ae4f371c09
ashification <aislinglee@gmail.com>
2021-04-15 23:14:39 +0100

    Adding publisher script

--- CA2_BuildRun_gpp.sh
+++ CA2_BuildRun_gpp.sh
@@ -4,9 +4,9 @@ if [ "$1" == "" ]; then
     printf  "ERROR: First Arg is empty, Please declare script name"
 else  
 	printf  "Building Script Please wait...\n"
-	g++ $1 -o ca1_script_$2 -lwiringPi 
+	g++ $1 -o ca2_script_$2 -lpaho-mqtt3c 
 	printf  "Running Script now....\n\n\n"
-	./ca1_script_$2
+	./ca2_script_$2
 	printf "\n\n"
 	printf "Script End\n"
 fi
